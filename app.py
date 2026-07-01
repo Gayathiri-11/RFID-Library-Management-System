7cd0757b7db25300f30088669af1848b24da546e
Shobika <sshobika006@gmail.com>
2026-07-01 22:23:04 +0530

    dashboard page

--- app.py
+++ app.py
@@ -81,24 +81,68 @@ def dashboard():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
 
+    # Total Books
     cursor.execute("SELECT COUNT(*) AS total FROM books")
     total_books = cursor.fetchone()["total"]
 
+    # Available Books
     cursor.execute("SELECT COUNT(*) AS total FROM books WHERE status='Available'")
     available_books = cursor.fetchone()["total"]
 
+    # Issued Books
     cursor.execute("SELECT COUNT(*) AS total FROM books WHERE status='Issued'")
     issued_books = cursor.fetchone()["total"]
 
+    # Students
     cursor.execute("SELECT COUNT(*) AS total FROM students")
     total_students = cursor.fetchone()["total"]
 
+    # Overdue Books
     cursor.execute("""
-        SELECT uid, roll_number, status, issue_date
+        SELECT COUNT(*) AS total
         FROM transactions
-        ORDER BY issue_date DESC
+        WHERE status='Issued'
+        AND due_date < CURDATE()
+    """)
+    overdue_books = cursor.fetchone()["total"]
+
+    # Total Fine (₹5 per overdue day)
+    cursor.execute("""
+        SELECT
+        IFNULL(SUM(GREATEST(DATEDIFF(CURDATE(), due_date),0)*5),0) AS total_fine
+        FROM transactions
+        WHERE status='Issued'
+        AND due_date < CURDATE()
+    """)
+    total_fine = cursor.fetchone()["total_fine"]
+
+    # Recent Transactions
+    cursor.execute("""
+        SELECT
+
+            b.book_name,
+
+            s.student_name,
+
+            t.status,
+
+            t.issue_date,
+
+            t.due_date
+
+        FROM transactions t
+
+        JOIN books b
+        ON t.uid=b.uid
+
+        JOIN students s
+        ON t.roll_number=s.roll_number
+
+        ORDER BY t.issue_date DESC
+
         LIMIT 10
     """)
+
     recent_transactions = cursor.fetchall()
 
     conn.close()
@@ -109,6 +153,8 @@ def dashboard():
         available_books=available_books,
         issued_books=issued_books,
         total_students=total_students,
+        overdue_books=overdue_books,
+        total_fine=total_fine,
         recent_transactions=recent_transactions
     )
 @app.route("/export_transactions")
