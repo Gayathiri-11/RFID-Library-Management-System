53ce5558b4f9f5ee057fc3bef3928d05ff2839a7
Shobika <sshobika006@gmail.com>
2026-07-01 21:39:37 +0530

    overdue page

--- app.py
+++ app.py
@@ -787,19 +787,34 @@ def inactive_students():
 @app.route("/overdue_report")
 def overdue_report():
 
-    department = request.args.get("department")
-    from_date = request.args.get("from_date")
-    to_date = request.args.get("to_date")
-    min_fine = request.args.get("min_fine")
-    max_fine = request.args.get("max_fine")
+    department = request.args.get("department", "")
+    from_date = request.args.get("from_date", "")
+    to_date = request.args.get("to_date", "")
+    min_fine = request.args.get("min_fine", "")
+    max_fine = request.args.get("max_fine", "")
 
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
 
     query = """
-        SELECT t.*, s.student_name, s.department
+        SELECT
+            t.uid,
+            t.roll_number,
+            s.student_name,
+            s.department,
+            t.issue_date,
+            t.due_date,
+            t.status,
+
+            GREATEST(DATEDIFF(CURDATE(), t.due_date),0) AS overdue_days,
+
+            GREATEST(DATEDIFF(CURDATE(), t.due_date),0) * 5 AS fine
+
         FROM transactions t
-        JOIN students s ON t.roll_number = s.roll_number
+
+        JOIN students s
+        ON t.roll_number = s.roll_number
+
         WHERE t.status='Issued'
         AND t.due_date < CURDATE()
     """
@@ -810,13 +825,23 @@ def overdue_report():
         query += " AND s.department=%s"
         params.append(department)
 
-    if from_date and to_date:
-        query += " AND t.due_date BETWEEN %s AND %s"
-        params.extend([from_date, to_date])
+    if from_date:
+        query += " AND t.due_date >= %s"
+        params.append(from_date)
 
-    if min_fine and max_fine:
-        query += " AND t.fine BETWEEN %s AND %s"
-        params.extend([min_fine, max_fine])
+    if to_date:
+        query += " AND t.due_date <= %s"
+        params.append(to_date)
+
+    if min_fine:
+        query += " AND (GREATEST(DATEDIFF(CURDATE(), t.due_date),0)*5) >= %s"
+        params.append(min_fine)
+
+    if max_fine:
+        query += " AND (GREATEST(DATEDIFF(CURDATE(), t.due_date),0)*5) <= %s"
+        params.append(max_fine)
+
+    query += " ORDER BY t.due_date ASC"
 
     cursor.execute(query, tuple(params))
 
@@ -836,18 +861,29 @@ def export_overdue_report():
 
     cursor.execute("""
         SELECT
+
             t.uid,
             t.roll_number,
             s.student_name,
             s.department,
             t.issue_date,
             t.due_date,
+
+            GREATEST(DATEDIFF(CURDATE(), t.due_date),0) AS overdue_days,
+
+            GREATEST(DATEDIFF(CURDATE(), t.due_date),0) * 5 AS fine,
+
             t.status
+
         FROM transactions t
+
         JOIN students s
-            ON t.roll_number = s.roll_number
+        ON t.roll_number=s.roll_number
+
         WHERE t.status='Issued'
-        AND t.due_date < CURDATE()
+        AND t.due_date<CURDATE()
+
+        ORDER BY t.due_date
     """)
 
     books = cursor.fetchall()
@@ -856,14 +892,11 @@ def export_overdue_report():
 
     df = pd.DataFrame(books)
 
-    file_name = "overdue_books_report.xlsx"
+    filename = "Overdue_Report.xlsx"
 
-    df.to_excel(file_name, index=False)
+    df.to_excel(filename,index=False)
 
-    return send_file(
-        file_name,
-        as_attachment=True
-    )
+    return send_file(filename,as_attachment=True)
 @app.route("/bulk_book_management")
 def bulk_book_management():
     return render_template("bulk_book_management.html")
