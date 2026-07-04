60abc25bf692e03bcec04b6d9e03969aa42494b3
Shobika <sshobika006@gmail.com>
2026-07-04 14:32:27 +0530

    login page

--- routes/auth.py
+++ routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,redirect
 from database.db import get_connection
 from datetime import datetime, timedelta
 
