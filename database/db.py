bea8354121141d899bf1e5f1b3e2961e84d13af8
Gayathiri-11 <23uree011@aaacet.ac.in>
2026-08-16 08:55:02 +0530

    Connect Render to TiDB Cloud

--- database/db.py
+++ database/db.py
@@ -1,9 +1,14 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Nanmai@2006",
-        database="RFID_LIBRARY"
+        host=os.environ.get("TIDB_HOST"),
+        port=int(os.environ.get("TIDB_PORT", "4000")),
+        user=os.environ.get("TIDB_USER"),
+        password=os.environ.get("TIDB_PASSWORD"),
+        database=os.environ.get("TIDB_DATABASE"),
+        ssl_ca=os.environ.get("TIDB_SSL_CA"),
+        ssl_verify_cert=True,
+        ssl_verify_identity=True
     )
